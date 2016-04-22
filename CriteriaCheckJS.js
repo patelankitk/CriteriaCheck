88dab5d74156e8487b5ae9c4bc804a43e1aef53e
patelankitk <patelankitk@yahoo.com>
2016-04-22 21:46:57 +0100

    change

--- CriteriaCheckJS.js
+++ CriteriaCheckJS.js
@@ -9,18 +9,34 @@ function CheckCriteria()
 {
     var UserAge = document.getElementById("age").value;
     
-    var UserHeight = document.getElementById("height").value;
-    
     var UserWeight = document.getElementById("weight").value;
     
-    if (UserWeight > 50 && UserWeight < 90 )//UserAge < 26) && (UserWeight > 50 && UserWeight < 90 )&& (UserHeight > 150 && UserHeight < 190))
+    var UserHeight = document.getElementById("height").value;
+    
+    
+    
+    if ((UserAge < 26) && (UserWeight > 50 && UserWeight < 90 )&& (UserHeight > 150 && UserHeight < 190))
         {
             // Displaying the result for Underweight //
-            document.getElementById("criteriacheck").innerHTML ="<li>"+ "You have satisfactorily met the criteria and you can proceed to booking this course."+"</li>";
+            document.getElementById("criteriacheck").innerHTML ="<li>"+ "You have satisfactorily met the criteria and you can proceed to booking    this course."+"</li>";
         }
+    
+    // If Age more then 25
     else
     {
-        document.getElementById("criteriacheck").innerHTML ="<li>"+ "No"+"</li>";
+              
+        if(UserAge>=26)
+            {
+                document.getElementById("criteriacheckage").innerHTML ="<li>"+ "Sorry you do not satisfy our minimum required criteria, your Age exceeds out criteria of Maximum 25, your age is "+UserAge+"."+"</li>"+"<br>";           
+            }
+        if(UserWeight <50 || UserWeight >90)
+            {
+                document.getElementById("criteriacheckweight").innerHTML ="<li>"+ "Sorry you do not satisfy our minimum required criteria,your Weight is outside our range of 50 to 90 kg, your weight is "+UserWeight+"."+"</li>"+"<br>";
+            }
+        if(UserHeight <150 || UserHeight >190)
+            {
+                document.getElementById("criteriacheckheight").innerHTML ="<li>"+"You do not satisfy our criteria, your Height is outside our range of 150 to 190 cm, your height is "+UserHeight+"."+"</li>"+"<br>";
+            }
     }
 }
 
