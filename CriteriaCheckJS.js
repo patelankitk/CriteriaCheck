9e89c4734da6f80f7871efa62965957e2b8bfb77
patelankitk <patelankitk@yahoo.com>
2016-04-25 09:32:18 +0100

    Check

--- CriteriaCheckJS.js
+++ CriteriaCheckJS.js
@@ -1,16 +1,17 @@
 // creating Event Listner on Click of Button to get the age from user //
-var eventAge = document.getElementById("age");
-eventAge.addEventListener('click',CheckCriteria1,false);
+var event1 = document.getElementById("age");
+event1.addEventListener('click', CheckCriteria,false);
 
 // creating Event Listner on Click of Button to get the Height from user //
-var eventHeight= document.getElementById("height");
-eventHeight.addEventListener('click',CheckCriteria2,false);
+var event2 = document.getElementById("height",false);
+event2.addEventListener('click', CheckCriteria);
 
 // creating Event Listner on Click of Button to get the Weight from user //
-var eventWeight = document.getElementById("weight");
-eventWeight.addEventListener('click',CheckCriteria,false);
+var event3 = document.getElementById("weight");
+event3.addEventListener('click', CheckCriteria,false);
 
 
+    
 function CheckCriteria()
 {
     var UserAge = document.getElementById("age").value;
@@ -19,23 +20,14 @@ function CheckCriteria()
     
     var UserWeight = document.getElementById("weight").value;
     
-    if (UserWeight > 50 && UserWeight < 90 )//UserAge < 26) && (UserWeight > 50 && UserWeight < 90 )&& (UserHeight > 150 && UserHeight < 190))
+    if (UserAge < 26 && (UserWeight > 50 && UserWeight < 90 )&& (UserHeight > 150 && UserHeight < 190))
         {
             // Displaying the result for Underweight //
             document.getElementById("criteriacheck").innerHTML ="<li>"+ "You have satisfactorily met the criteria and you can proceed to booking this course."+"</li>";
         }
-    else
+    else if
     {
         document.getElementById("criteriacheck").innerHTML ="<li>"+ "No"+"</li>";
     }
 }
 
-function CheckCriteria1()
-{
-    
-}
-
-function CheckCriteria2()
-{
-    
-}
